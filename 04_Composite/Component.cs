2f8c414c8e860fdf5a369bc4321c03c3fd3365f6
ali rezaei <alirezai.pro@gmail.com>
2024-07-10 02:46:35 +0330

    feat: add composite design pattern

---
+++ 04_Composite/Component.cs
@@ -0,0 +1,75 @@
+﻿namespace _04_Composite;
+
+public abstract class Component
+{
+    protected string _name;
+
+    protected Component(string name)
+    {
+        _name = name;
+    }
+
+    public abstract void Add(Component component);
+
+    public abstract void Remove(Component component);
+
+    public abstract void Display(int depth);
+}
+
+public class Composite : Component
+{
+    private List<Component> _components = new List<Component>();
+
+    public Composite(string name) : base(name)
+    {
+    }
+
+    public Composite(string name, Component[] components) : base(name)
+    {
+        foreach (var item in components)
+        {
+            Add(item);
+        }
+    }
+
+    public override void Add(Component component)
+    {
+        _components.Add(component);
+    }
+
+    public override void Remove(Component component)
+    {
+        _components.Remove(component);
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + _name);
+        foreach (var item in _components)
+        {
+            item.Display(depth + 2);
+        }
+    }
+}
+
+public class Leaf : Component
+{
+    public Leaf(string name) : base(name)
+    {
+    }
+
+    public override void Add(Component component)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Remove(Component component)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Display(int depth)
+    {
+        Console.WriteLine(new string('-', depth) + _name);
+    }
+}
---
+++ 04_Composite/Program.cs
@@ -0,0 +1,21 @@
+﻿using _04_Composite;
+
+Component component = new Composite("Root Item", new Component[]
+{
+    new Leaf("Leaf Item 1"),
+    new Leaf("Leaf Item 2"),
+    new Composite("Composite 1", new Component[]
+    {
+        new Leaf("Leaft Item c 1-1"),
+        new Leaf("Leaft Item c 1-2"),
+        new Leaf("Leaft Item c 1-3"),
+        new Composite("Composite 1-1", new Component[]
+        {
+            new Leaf("Leaf Item 1-1-1")
+        })
+    }),
+    new Leaf("Leaf Item 3"),
+    new Leaf("Leaf Item 4"),
+});
+
+component.Display(1);
