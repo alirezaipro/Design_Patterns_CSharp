84280eef0925bc89b7e5133bcac3bae5b03a32cc
ali rezaei <alirezai.pro@gmail.com>
2024-08-22 01:24:01 +0330

    feat: add prototype design pattern

---
+++ 07_Prototype/IPrototype.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_Prototype
+{
+    public interface IPrototype
+    {
+        IPrototype Clone();
+    }
+
+    public class ConcreatPrototype1 : IPrototype
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public IPrototype Clone()
+        {
+            var result=this.MemberwiseClone();
+            return (ConcreatPrototype1)result;
+        }
+    }
+
+    public class ConcreatPrototype2 : IPrototype
+    {
+        public int Id { get; set; }
+
+        public ConcreatPrototype3 Data { get; set; }
+
+        public ConcreatPrototype2()
+        {
+            Data = new ConcreatPrototype3();
+        }
+
+
+        public IPrototype Clone()
+        {
+            var result = (ConcreatPrototype2)MemberwiseClone();
+
+            result.Data = (ConcreatPrototype3)Data.Clone();
+
+            return result;
+        }
+    }
+
+    public class ConcreatPrototype3 : IPrototype
+    {
+
+        public string FullName { get; set; }
+
+        public string NationalCode { get; set; }
+
+        public IPrototype Clone()
+        {
+            var result = this.MemberwiseClone();
+            return (ConcreatPrototype3)result;
+        }
+    }
+}
---
+++ 07_Prototype/Program.cs
@@ -0,0 +1,17 @@
+﻿using _07_Prototype;
+
+//Shallow Copy
+ConcreatPrototype1 concreatPrototype1 = new ConcreatPrototype1();
+ConcreatPrototype1 concreatPrototype1_2 = (ConcreatPrototype1)concreatPrototype1.Clone();
+
+Console.WriteLine("Shallow Copy:");
+Console.WriteLine(concreatPrototype1==concreatPrototype1_2);
+
+//Deep Copy
+ConcreatPrototype2 concreatPrototype2 = new ConcreatPrototype2();
+ConcreatPrototype2 concreatPrototype2_2 = (ConcreatPrototype2)concreatPrototype2.Clone();
+
+Console.WriteLine("---------------");
+Console.WriteLine("Deep Copy:");
+Console.WriteLine(concreatPrototype2 == concreatPrototype2_2);
+Console.WriteLine(concreatPrototype2.Data == concreatPrototype2_2.Data);
