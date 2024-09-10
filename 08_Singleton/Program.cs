18e7f77a8d83be63fe595fc2fe6f5a603429fe43
ali rezaei <alirezai.pro@gmail.com>
2024-09-10 23:07:21 +0330

    feat: add singleton design pattern

---
+++ 08_Singleton/Program.cs
@@ -0,0 +1,6 @@
+﻿using _08_Singleton;
+
+Singleton singleton_01 = Singleton.GetInstance();
+Singleton singleton_02 = Singleton.GetInstance();
+
+Console.WriteLine(singleton_01 == singleton_02);
---
+++ 08_Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Singleton
+{
+    internal class Singleton
+    {
+        private static Singleton instance = new Singleton();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            return instance;
+        }
+    }
+}
