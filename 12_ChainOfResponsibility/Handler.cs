5867ad010e9122324ddcd74514b05e539f5656c8
Ali Rezaei <alirezai.pro@gmail.com>
2024-11-20 21:29:03 +0330

    feat: add chain of resposibility design pattern

---
+++ 12_ChainOfResponsibility/Handler.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_ChainOfResponsibility
+{
+    internal abstract class Handler
+    {
+        protected Handler successor;
+
+        public void SetSuccessor(Handler successor)
+        {
+            this.successor = successor;
+        }
+
+        public abstract void HandleRequest(int request);
+    }
+
+    internal class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine($"ConcreteHandler1.HandleRequest() with value: {request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    internal class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine($"ConcreteHandler2.HandleRequest() with value: {request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+
+    internal class ConcreteHandler3 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 20 && request < 30)
+            {
+                Console.WriteLine($"ConcreteHandler3.HandleRequest() with value: {request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
---
+++ 12_ChainOfResponsibility/Program.cs
@@ -0,0 +1,17 @@
+﻿using _12_ChainOfResponsibility;
+
+int[] requests = [2, 5, 14, 22, 18, 3, 27, 20];
+
+Handler handler_01 = new ConcreteHandler1();
+Handler handler_02 = new ConcreteHandler2();
+Handler handler_03 = new ConcreteHandler3();
+
+handler_01.SetSuccessor(handler_02);
+handler_02.SetSuccessor(handler_03);
+
+foreach(int i in requests)
+{
+   handler_01.HandleRequest(i);
+}
+
+Console.ReadLine();
