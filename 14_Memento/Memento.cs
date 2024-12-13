4957c8a1eee7e26bb72bfb0dce38be0e20d83deb
Ali Rezaei <alirezai.pro@gmail.com>
2024-12-13 23:17:41 +0330

    feat: add memento design pattern

---
+++ 14_Memento/Memento.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Memento
+{
+    public class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            _state = state;
+        }
+
+        public string State
+        {
+            get { return _state; }
+        }
+    }
+
+    public class Originator
+    {
+        string _state;
+
+        public string State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                Console.WriteLine($"Originator state is : {_state}");
+            }
+
+        }
+
+        public Memento CreateMemento()
+        {
+            return new Memento(_state);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine($"Set memento is called");
+            State = memento.State;
+        }   
+    }
+
+    public class Caretaker
+    {
+        Memento memento;
+
+        public Memento Memento
+        {
+            set { memento = value; }
+            get { return memento; }
+        }
+    }
+}
+
---
+++ 14_Memento/Program.cs
@@ -0,0 +1,14 @@
+﻿using _14_Memento;
+
+Originator originator= new Originator();
+originator.State = "On";
+
+Caretaker caretaker= new Caretaker();
+
+caretaker.Memento= originator.CreateMemento();
+
+originator.State = "Off";
+
+originator.SetMemento(caretaker.Memento);
+
+Console.WriteLine("Finish");
