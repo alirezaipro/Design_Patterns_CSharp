4634880411862f164104e38c077af169b14770b6
ali rezaei <alirezai.pro@gmail.com>
2025-02-19 03:27:52 +0330

    add interpreter design pattern

---
+++ 20_Interpreter/AbstractExpression.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Interpreter
+{
+    internal abstract class AbstractExpression
+    {
+        public abstract void Interpret(Context context);
+    }
+
+    internal class TerminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("TerminalExpression.Interpret()");
+        }
+    }
+
+    internal class NonterminalExpression : AbstractExpression
+    {
+        public override void Interpret(Context context)
+        {
+            Console.WriteLine("NonterminalExpression.Interpret()");
+        }
+    }
+}
---
+++ 20_Interpreter/Program.cs
@@ -0,0 +1,16 @@
+﻿using _20_Interpreter;
+
+Context context=new Context();
+
+List<AbstractExpression> expressions=new List<AbstractExpression>();
+expressions.Add(new TerminalExpression());
+expressions.Add(new NonterminalExpression());
+expressions.Add(new NonterminalExpression());
+expressions.Add(new TerminalExpression());
+
+foreach(AbstractExpression expression in expressions)
+{
+    expression.Interpret(context);
+}
+
+Console.ReadLine();
